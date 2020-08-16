354e31d35948a3c54571196b848b04a91358b540
PeterUpfold <pgithub@upfold.org.uk>
2020-08-16 09:02:48 +0100

    Adding some users configurations

---
+++ modules/pz_base/manifests/init.pp
@@ -0,0 +1,17 @@
+# Base software packages and common configurations to all system roles
+class pz_base {
+  $base_packages = [ 'build-essential', 'curl', 'wget', 'screen', 'tmux', 'unattended-upgrades', 'git', 'logrotate', 'rsync', 'vim' ]  
+
+  package { $base_packages:
+    ensure => latest
+  }
+  # SSH configuration
+  file { '/etc/ssh/sshd_config':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => ('puppet:///modules/pz_base/sshd_config')
+  }
+
+}
--- modules/pz_users/manifests/init.pp
+++ modules/pz_users/manifests/init.pp
@@ -6,8 +6,16 @@ class pz_users {
   }
 
   user { 'peter':
+    ensure     => present,
+    uid        => 1001,
+    groups     => [ 'peter', 'adm', 'sudo' ],
+    membership => minimum,
+    home       => '/home/peter',
+  }
+
+  group { 'peter':
     ensure => present,
-    uid    => 1001,
+    gid    => 1001,
   }
 
   file { '/home/peter':
@@ -15,6 +23,29 @@ class pz_users {
     owner  => 'peter',
     group  => 'peter',
     mode   => '0700'
+  }
+
+  file { '/home/peter/.ssh':
+    ensure => directory,
+    owner  => 'peter',
+    group  => 'peter',
+    mode   => '0700'
+  }
+
+  file { '/home/peter/.ssh/authorized_keys':
+    ensure => file,
+    owner  => 'peter',
+    group  => 'peter',
+    mode   => '0600',
+    source => ('puppet:///modules/pz_users/peter.authorized_keys')
+  }
+ 
+  file { '/etc/sudoers.d/015_peter-nopasswd':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0440',
+    source => ('puppet:///modules/pz_users/015_peter-nopasswd')
   }
 
 }
